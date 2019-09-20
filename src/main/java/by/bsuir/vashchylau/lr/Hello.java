77a97f59682372fd5985aecd85cf622c2cee2f90
neverfoggy <neverfoggy1@gmail.com>
2019-09-20 10:35:34 +0300

    second commit

--- src/main/java/by/bsuir/vashchylau/lr/Hello.java
+++ src/main/java/by/bsuir/vashchylau/lr/Hello.java
@@ -2,7 +2,7 @@ package main.java.by.bsuir.vashchylau.lr;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("First lab!!!");
     }
 }
 
